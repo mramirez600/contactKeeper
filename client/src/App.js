9d089affd8d42d084f3b1ae8df46d67a9de819cf
Marvin Ramirez <eldo@hotmail.com>
2019-11-28 21:03:26 -0500

    user registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -7,25 +8,29 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/authState';
+import AlertState from './context/alert/alertState';
 import './App.css';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alert />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
-import AuthContext from '../auth/authContact';
-import authReducer from '../auth/authReducer';
+import axios from 'axios';
+import AuthContext from './authContext';
+import authReducer from './authReducer';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -24,14 +25,38 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialstate);
 
   // load user
+  const loadUser = () => console.log('loaduser');
 
   // register user
+  const register = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    try {
+      const res = await axios.post('/api/users', formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
 
   // login user
+  const login = () => console.log('login');
 
   // logout
+  const logout = () => console.log('logout');
 
   // clear errors
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
     <AuthContext.Provider
@@ -40,7 +65,12 @@ const AuthState = props => {
         isAuthenticated: state.isAuthenticated,
         loading: state.loading,
         user: state.user,
-        error: state.error
+        error: state.error,
+        register,
+        login,
+        logout,
+        loadUser,
+        clearErrors
       }}
     >
       {props.children}
