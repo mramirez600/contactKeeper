50f554251ce25f918ce1782f2e075b3d5e604dda
Marvin Ramirez <eldo@hotmail.com>
2019-11-30 13:52:36 -0500

    update contact commit

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -52,8 +52,14 @@ const ContactState = props => {
     }
   };
   // Delete contact
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   // clear contacts
@@ -66,14 +72,29 @@ const ContactState = props => {
     dispatch({ type: SET_CURRENT, payload: contact });
   };
 
+  // update contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   // clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // filter contacts
